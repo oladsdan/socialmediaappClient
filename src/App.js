77b099c2f5e0bd74f1ad788f67fbeeccf8f2dc9f
oladsdan <104092480+oladsdan@users.noreply.github.com>
2023-08-17 21:54:53 +0100

    redux index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from 'containers/homePage';
+import LoginPage from 'containers/loginPage';
+import ProfilePage from 'containers/profilePage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId"  element={<ProfilePage />} />
+
+        </Routes>
+      
+      </BrowserRouter>
+
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import authReducer from "./reduxState";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist"
+import { Storage } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +26,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
