c8f0860e42a84ffb40975dd83b52d0a56ab0ab88
oladsdan <104092480+oladsdan@users.noreply.github.com>
2023-08-18 12:27:41 +0100

    configring mui

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import './App.css';
 import HomePage from 'containers/homePage';
 import LoginPage from 'containers/loginPage';
 import ProfilePage from 'containers/profilePage';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from 'theme';
 
 
 function App() {
+
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode), [mode]))
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId"  element={<ProfilePage />} />
-
-        </Routes>
+        <ThemeProvider theme= {theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId"  element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       
       </BrowserRouter>
 
